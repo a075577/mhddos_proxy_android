36a356cf23181917ff893eb62db4a506c47af5a0
a075577 <a07557766@gmail.com>
2025-01-14 17:58:07 +0300

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -1,2 +1,8 @@
 
 mkdir -p .shortcuts .termux/boot
+
+apt update -y && apt install -y root-repo tsu glibc-repo glibc
+curl -s https://github.com/porthole-ascend-cinnamon/mhddos_proxy_releases/releases/latest/download/mhddos_proxy_linux_arm64 -Lo mhddos_proxy
+grun -c mhddos_proxy
+curl -sOL https://github.com/a075577/mhddos_proxy_android/raw/refs/heads/main/mhddos_proxy.sh
+chmod +x mhddos_proxy mhddos_proxy.sh
