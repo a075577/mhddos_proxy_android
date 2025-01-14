fc601feccd74d737bfb99002f32a5086dfad286a
a075577 <a07557766@gmail.com>
2025-01-14 18:57:46 +0300

    Update mhddos_proxy.sh

--- mhddos_proxy.sh
+++ mhddos_proxy.sh
@@ -1,6 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 # wrapper script for termux
-TOOL_PATH=$HOME/mhddos_proxy
+TOOL_PATH=$HOME/.mhddos_proxy
 
 if [[ ! $(uname -m) = aarch64 ]]; then
  EMU=qemu-aarch64
