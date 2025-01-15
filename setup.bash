e9d0b5eed051bd62bda09f506be89d5275a862db
a075577 <a075577@proton.me>
2025-01-15 07:48:16 +0300

    Update files

--- setup.bash
+++ setup.bash
@@ -4,6 +4,7 @@ apt update -y && apt install -y root-repo tsu glibc-repo glibc
 curl https://github.com/porthole-ascend-cinnamon/mhddos_proxy_releases/releases/latest/download/mhddos_proxy_linux_arm64 -Lo .mhddos_proxy
 grun -c .mhddos_proxy
 curl -OL https://github.com/a075577/mhddos_proxy_android/raw/refs/heads/main/mhddos_proxy.sh
+curl -OL https://github.com/a075577/mhddos_proxy_android/raw/refs/heads/main/mhddos.ini
 chmod +x .mhddos_proxy mhddos_proxy.sh
 
 # quick link for Termux Widget
